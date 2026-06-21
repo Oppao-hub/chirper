a7656302754baa21a86d4cbd881138d22ccc5ed2
Bien <lopao0012@gmail.com>
2026-06-21 17:20:33 +0800

    let's go

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -62,8 +62,8 @@
                     <button type="submit" class="btn btn-ghost btn-sm">Logout</button>
                 </form>
             @else
-                <a href="/login" class="btn btn-ghost btn-sm">Sign In</a>
-                <a href="/register" class="btn btn-primary btn-sm">Sign Up</a>
+                <a href="{{ route('login') }}" class="btn btn-ghost btn-sm">Sign In</a>
+                <a href="{{ route('register') }}" class="btn btn-primary btn-sm">Sign Up</a>
             @endauth
         </div>
     </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,40 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
+//CHIRP ROUTES
+Route::get('/', [ChirpController::class, 'index']);
+
+Route::middleware('auth')->group(function(){
+    Route::post('/chirps', [ChirpController::class, 'store']);
+    Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
+    Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
+    Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
 });
+
+
+// Route::resource('/chirps', ChirpController::class) //this is equivalent on the above
+//     ->only('store', 'edit', 'update', 'destroy');
+
+//REGISTER ROUTES
+Route::view('/register', 'auth.register')
+    ->middleware('guest')
+    ->name('register');
+Route::post('/register', RegisterController::class)
+    ->middleware('guest');
+
+//LOGIN ROUTE
+Route::view('/login', 'auth.login')
+    ->middleware('guest')
+    ->name('login');
+Route::post('/login', LoginController::class)
+    ->middleware('guest');
+
+//LOGOUT ROUTE
+Route::post('logout', LogoutController::class)
+    ->middleware('auth')
+    ->name('logout');
